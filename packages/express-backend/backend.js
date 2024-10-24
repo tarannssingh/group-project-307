f4bbf03bd76729953d07aa670bea830f67e4487f
Taran Singh <tarannssingh@gmail.com>
2024-10-24 10:07:30 -0700

    feat: backend more complete

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors"
 
 const app = express();
 const port = 5478;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("hi")
