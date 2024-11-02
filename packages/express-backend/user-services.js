1742a12d6d17ba53e046da3011fd407bc6c20726
Gary Yim <89404326+Gary055@users.noreply.github.com>
2024-11-01 19:31:32 -0700

    updated to process Mongo URI

--- packages/express-backend/user-services.js
+++ packages/express-backend/user-services.js
@@ -1,49 +1,69 @@
 import mongoose from "mongoose";
 import userModel from "./user";
 
+import dotenv from "dotenv"
+
+dotenv.config();
+
 mongoose.set("debug", true);
 
 mongoose
-  .connect("mongodb://localhost:27017/users", {
+  .connect(
+    process.env.MONGODB_URI,
+    // "mongodb://localhost:27017/users", 
+    {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .catch((error) => console.log(error));
 
-function getUsers(name, job) {
-  let promise;
-  if (name === undefined && job === undefined) {
-    promise = userModel.find();
-  } else if (name && !job) {
-    promise = findUserByName(name);
-  } else if (job && !name) {
-    promise = findUserByJob(job);
+  async function findUserById(id) {
+    try {
+      return await userModel.findById(id);
+    } catch (error) {
+      console.log(error);
+      return undefined;
+    }
   }
-  return promise;
-}
-
-function findUserById(id) {
-  return userModel.findById(id);
-}
-
-function addUser(user) {
-  const userToAdd = new userModel(user);
-  const promise = userToAdd.save();
-  return promise;
-}
-
-function findUserByName(name) {
-  return userModel.find({ name: name });
-}
-
-function findUserByJob(job) {
-  return userModel.find({ job: job });
-}
-
-export default {
-  addUser,
-  getUsers,
-  findUserById,
-  findUserByName,
-  findUserByJob,
-};
+  
+  async function addUser(user) {
+    try {
+      const userToAdd = new userModel(user);
+      const savedUser = await userToAdd.save();
+      return savedUser;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+  
+  async function findUserByName(name) {
+    return await userModel.find({ name: name });
+  }
+  
+  async function findUserByJob(job) {
+    return await userModel.find({ job: job });
+  }
+  
+  async function findUserByNameAndJob(name, job) {
+    return await userModel.find({ name: name, job: job });
+  }
+  
+  async function deleteUser(id) {
+    return await userModel.findByIdAndDelete(id);
+  }
+  
+  // async function disconnectDB() {
+  //   await mongoose.connection.close();
+  //   await mongoose.disconnect();
+  // }
+  
+  export default {
+    addUser,
+    getUsers,
+    findUserById,
+    findUserByName,
+    findUserByJob,
+  };
+  
+  // exports.disconnectDB = disconnectDB;
