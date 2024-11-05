40aae7d5160632c7bb15e7c088d154b7a0011510
Gary Yim <89404326+Gary055@users.noreply.github.com>
2024-11-05 10:37:54 -0800

    added password strength function

--- packages/express-backend/password-servicies.js
+++ packages/express-backend/password-servicies.js
@@ -2,9 +2,9 @@ import bcrypt from "bcrypt"
 
 const saltRounds = 10;
 const password = 'very_great_password'
+const bad_password = 'very_bad_password'
 const ex1 = "$2b$10$6W8FOgmppDxBsmAXYfGdJeVa7lbnEj/l2DDY4tOfXuyzkQNwryj7O"
 const ex2 = "$2b$10$N8V8b./3rYHSMJpOL9MdmeoqulwB2JeztdYiU8pF59oB7O0ir7BOO"
-const bad_password = 'very_bad_password'
 const ex3 = "$2b$10$lqGB8oNU.2SV/j8CJGKqKOKUvFIfkB.BQlz8WxvIP.fRgtW87mJzS"
 
 // bcrypt.genSalt(saltRounds, (error, salt) => {
--- packages/express-backend/user-services.js
+++ packages/express-backend/user-services.js
@@ -1,3 +1,5 @@
+import {body, validationResult} from "express-validator";
+
 // import mongoose from "mongoose";
 // // import userModel from "./user";
 
@@ -16,9 +18,6 @@
 //     useUnifiedTopology: true,
 //   })
 //   .catch((error) => console.log(error));
-
-
-  import {body, validationResult} from "express-validator"
 
 const signupValidators = [
     body("email").isEmail().withMessage("Please enter a valid email"),
@@ -46,4 +45,45 @@ const login = async (email, password) => {
 
 }
 
-export default {signupValidators, signup}
+function passwordStrength(pw) {
+    const criteria = {
+        length: false,
+        uppercase: false,
+        lowercase: false,
+        number: false,
+        speChar: false,
+    };
+
+    if(pw.length >= 10) {
+        criteria.length = true;
+    }
+    if(/[A-Z]/.test(pw)) {
+        criteria.uppercase = true;
+    }
+    if(/[a-z]/.test(pw)) {
+        criteria.lowercase = true;
+    }
+    if(/\d/.test(pw)) {
+        criteria.number = true;
+    }
+    if(/[$@!%*?&]/.test(pw)) {
+        criteria.speChar = true;
+    }
+    const met = Object.values(criteria).filter(Boolean).length;
+    switch(met) {
+        case 1:
+            return "Weak";
+        case 2:
+            return "Middling";
+        case 3:
+            return "Moderate"
+        case 4:
+            return "Strong"
+        case 5:
+            return "Very Strong"
+        default:
+            return "Invalid"
+    }
+}
+
+export default {signupValidators, signup, passwordStrength};
